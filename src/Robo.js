3cc4a719cff716138cac292f48800748cc774017
KANDHVE950 <kandhve@iitg.ac.in>
2019-12-16 12:03:08 +0530

    updated

--- src/Robo.js
+++ src/Robo.js
@@ -8,7 +8,7 @@ const Cards = (props) => {
          <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-S">
            <img  src={`https://robohash.org/${id}?100*100`} />
            <div>
-            <h1>{name}</h1>
+            <p>{name}</p>
             <p>{username}</p>
             <p>{email}</p>
            </div>
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 
-const  SearchBox = () => {
+const  SearchBox = ({searchChange}) => {
 
 	return (
         <div className="pa2">
-        	<input className="pa1 bg-lightest-blue" type="search" placeholder="items" />
+        	<input className="pa1 bg-lightest-blue"
+        	 type="search"
+        	 placeholder="search robots"
+        	 onChange={searchChange} />
+
         </div>
 		);
 }
